47bef40ea4cef62ef74c997135311aebfa0f50a2
gamabarbara <barbara.cp@outlook.com>
2022-08-05 09:42:32 -0300

    Esqueleto HTML página principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./principal/principal.module').then(m => m.PrincipalModule)
+  },
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
--- src/app/principal/principal.module.ts
+++ src/app/principal/principal.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomeComponent } from './components/home/home.component';
+import { PrincipalRoutingModule } from './principal-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    NavbarComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    PrincipalRoutingModule
   ]
 })
 export class PrincipalModule { }
