fd3ae2e5f45b1dbbad4cbaae8cb8e88c65b41522
gamabarbara <barbara.cp@outlook.com>
2022-08-09 09:39:10 -0300

    Dialog da Página Principal

---
+++ src/app/principal/components/dialog-principal/dialog-principal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-principal',
+  templateUrl: './dialog-principal.component.html',
+  styleUrls: ['./dialog-principal.component.css']
+})
+export class DialogPrincipalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
