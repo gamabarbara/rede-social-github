c5301b6a37d074527a161ab903856f7a0895bac1
Matheusoro <matheusoro66@gmail.com>
2022-08-09 13:10:44 -0300

    Criação de barra na navbar

--- src/app/feed/feed.module.ts
+++ src/app/feed/feed.module.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { FeedRoutingModule } from './feed-routing.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
 import { CardsComponent } from './components/cards/cards.component';
+import { MaterialModule } from '../material/material.module';
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    NavbarComponent,
     CardsComponent
   ],
   imports: [
     CommonModule,
-    FeedRoutingModule
+    FeedRoutingModule,
+    MaterialModule
   ]
 })
 export class FeedModule { }
