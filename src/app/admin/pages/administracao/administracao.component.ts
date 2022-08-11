c97471fc7ec2a7a6d8c918832668cb64b19525cb
Carol <carolqueiroz96@outlook.com.br>
2022-08-11 16:06:49 -0300

    Ajustes complementares do Feed

--- src/app/admin/pages/administracao/administracao.component.ts
+++ src/app/admin/pages/administracao/administracao.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { LoginGitComponent } from 'src/app/auth/pages/login-git/login-git.component';
 
 @Component({
-  selector: 'app-administracao',
+  selector: 'app-administracao', 
   templateUrl: './administracao.component.html',
-  styleUrls: ['./administracao.component.css']
+  styleUrls: ['./administracao.component.css'],
+  
 })
 export class AdministracaoComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(LoginGitComponent, {
+      width: '400px',
+      height: '350px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
   }
 
 }
