167226d2f75e29bed63b2dfb3a60eeeda7c3a141
gamabarbara <barbara.cp@outlook.com>
2022-08-15 11:10:40 -0300

    Adição de login admin com Google

--- src/app/admin/components/login-admin/login-admin.component.ts
+++ src/app/admin/components/login-admin/login-admin.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { ServicesService } from '../../services/services.service';
 
 @Component({
   selector: 'app-login-admin',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private adminService: ServicesService,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  signInWithGoogle() {
+    this.adminService.signInWithGoogle()
+      .subscribe(
+        data => {
+          /* console.log(data) */
+          this.snackbar.open('Register complete', 'Ok', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+
+          })
+          this.dialog.closeAll()
+          this.router.navigateByUrl('/feed')
+        },
+        error => {
+          this.snackbar.open('Erro ao cadastrar com o Google', 'Ok', {
+            duration: 3000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top'
+          })
+        }
+      )
   }
 
 }
--- src/app/admin/services/services.service.ts
+++ src/app/admin/services/services.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { GoogleAuthProvider } from 'firebase/auth';
+import { from, tap } from 'rxjs';
+import { admin } from 'src/app/admin/models/admin'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
 
-  constructor() { }
+  private adminCollection = this.store.collection<admin>('admin')
+
+  constructor(
+    private authentication: AngularFireAuth,
+    private store: AngularFirestore
+  ) { }
+
+signInWithGoogle() {
+    const googleProvider = new GoogleAuthProvider();
+
+    return from(this.authentication.signInWithPopup(googleProvider))
+      .pipe(
+        tap((credentials) => {
+          const uid = credentials.user?.uid as string
+
+          const photoURL = credentials.user?.photoURL as string
+          const email = credentials.user?.email as string
+
+          this.adminCollection.doc(uid).set({
+            uid: uid,
+            photoURL: photoURL,
+            email: email
+          })
+        })
+      )
+  }
+
+  signOut() {
+    this.authentication.signOut()
+  }
+
 }
