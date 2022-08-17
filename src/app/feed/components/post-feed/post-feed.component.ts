523dc9802561c590104842e885af1c8578ea8f90
gamabarbara <barbara.cp@outlook.com>
2022-08-17 09:36:15 -0300

    Funcionalidade botão login admi

--- src/app/feed/components/post-feed/post-feed.component.ts
+++ src/app/feed/components/post-feed/post-feed.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { CreatePostComponent } from '../create-post/create-post.component';
-
+import {FirebaseTSFirestore, Limit, OrderBy, Where} from 'firebasets/firebasetsFirestore/firebaseTSFirestore'
 @Component({
   selector: 'app-post-feed',
   templateUrl: './post-feed.component.html',
   styleUrls: ['./post-feed.component.css']
 })
 export class PostFeedComponent implements OnInit {
-
+  firestore = new FirebaseTSFirestore();
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
@@ -16,5 +16,20 @@ export class PostFeedComponent implements OnInit {
   onCreatePostClick() {
     this.dialog.open(CreatePostComponent)
   }
+/*   getPosts() {
+    this.firestore.getCollection(
+      {
+        path: ["Posts"],
+        where: [
+          new OrderBy("timestamp", "desc"),
+          new Limit(10)
+        ],
+        onComplete: (result) => {
+
+        },
+        onFail: error => 
+      }
+    )
+  } */
 
 }
