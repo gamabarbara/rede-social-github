a1a27dcc1cf2b3656557e2b1523116192e47b012
gamabarbara <barbara.cp@outlook.com>
2022-08-26 20:00:47 -0300

    Deploy da aplicação

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyBOArA3Qz9gdDfU9Uq4J4XZionlaWaPtJw",
+    authDomain: "rede-social-52847.firebaseapp.com",
+    projectId: "rede-social-52847",
+    storageBucket: "rede-social-52847.appspot.com",
+    messagingSenderId: "87853436121",
+    appId: "1:87853436121:web:f4a84ec26c603fd0c542c8",
+    measurementId: "G-JL40FTV1G0"
+  }
 };
